917ae90157ad36c3c626a5973dba9e4177cff1a1
IreneGigabit <33124327+IreneGigabit@users.noreply.github.com>
2020-09-04 17:59:55 +0800

    Update SHtml.cs

--- App_Code/SHtml.cs
+++ App_Code/SHtml.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -23,6 +23,10 @@ public static class SHtml
 	public static string Option(DBHelper conn, string sql, string valueFormat, string textFormat) {
 		return Option(conn, sql, valueFormat, textFormat, "", true);
 	}
+    public static string Option(DBHelper conn, string sql, string valueFormat, string textFormat, string setValue) {
+        return Option(conn, sql, valueFormat, textFormat, "", true, setValue);
+    }
+
 
 	/// <summary>
 	/// 產生Option字串
@@ -36,6 +40,9 @@ public static class SHtml
 	public static string Option(DBHelper conn, string sql, string valueFormat, string textFormat, bool showEmpty) {
 		return Option(conn, sql, valueFormat, textFormat, "", showEmpty);
 	}
+    public static string Option(DBHelper conn, string sql, string valueFormat, string textFormat, bool showEmpty, string setValue) {
+        return Option(conn, sql, valueFormat, textFormat, "", showEmpty, setValue);
+    }
 
 	/// <summary>
 	/// 產生Option字串
@@ -45,40 +52,59 @@ public static class SHtml
 	/// <param name="valueFormat">option的value格式用{}包住欄位,ex:{scode}</param>
 	/// <param name="textFormat">option的文字格式用{}包住欄位,ex:{scode}_{sc_name}</param>
 	/// <param name="attrFormat">option的attribute格式用{}包住欄位,ex:value1='{scode1}'</param>
-	/// <param name="showEmpty">空白選項的顯示字串</param>
+    /// <param name="showEmpty">顯示「請選擇」</param>
 	/// <returns></returns>
-	public static string Option(DBHelper conn, string sql, string valueFormat, string textFormat, string attrFormat, bool showEmpty) {
-		Regex rgx = new Regex("{([^{}]+)}", RegexOptions.IgnoreCase);
-		string rtnStr = "";
+    public static string Option(DBHelper conn, string sql, string valueFormat, string textFormat, string attrFormat, bool showEmpty) {
+        return Option(conn, sql, valueFormat, textFormat, attrFormat, showEmpty, "");
+    }
 
-		//處理空白選項
-		if (showEmpty)
-			rtnStr += "<option value='' style='color:blue' selected>請選擇</option>\n";
+    /// <summary>
+    /// 產生Option字串
+    /// </summary> 
+    /// <param name="conn">DBHelper物件</param>
+    /// <param name="sql">SQL語法</param>
+    /// <param name="valueFormat">option的value格式用{}包住欄位,ex:{scode}</param>
+    /// <param name="textFormat">option的文字格式用{}包住欄位,ex:{scode}_{sc_name}</param>
+    /// <param name="attrFormat">option的attribute格式用{}包住欄位,ex:value1='{scode1}'</param>
+    /// <param name="showEmpty">顯示「請選擇」</param>
+    /// <param name="setValue">預設值</param>
+    /// <returns></returns>
+    public static string Option(DBHelper conn, string sql, string valueFormat, string textFormat, string attrFormat, bool showEmpty, string setValue) {
+        Regex rgx = new Regex("{([^{}]+)}", RegexOptions.IgnoreCase);
+        string rtnStr = "";
 
-		using (SqlDataReader dr = conn.ExecuteReader(sql)) {
-			while (dr.Read()) {
-				//處理value
-				string val = valueFormat;
-				foreach (Match match in rgx.Matches(valueFormat)) {
-					val = val.Replace(match.Value, dr.SafeRead(match.Result("$1"), ""));
-				}
+        //處理空白選項
+        if (showEmpty)
+            rtnStr += "<option value='' style='color:blue' selected>請選擇</option>\n";
 
-				//處理text
-				string txt = textFormat;
-				foreach (Match match in rgx.Matches(textFormat)) {
-					txt = txt.Replace(match.Value, dr.SafeRead(match.Result("$1"), ""));
-				}
+        using (SqlDataReader dr = conn.ExecuteReader(sql)) {
+            while (dr.Read()) {
+                //處理value
+                string val = valueFormat;
+                foreach (Match match in rgx.Matches(valueFormat)) {
+                    val = val.Replace(match.Value, dr.SafeRead(match.Result("$1"), ""));
+                }
 
-				//處理attribute
-				string attr = attrFormat;
-				foreach (Match match in rgx.Matches(attrFormat)) {
-					attr = attr.Replace(match.Value, dr.SafeRead(match.Result("$1"), ""));
-				}
-				rtnStr += "<option value='" + val + "' " + attr + ">" + txt + "</option>\n";
-			}
-		}
-		return rtnStr;
-	}
+                //處理text
+                string txt = textFormat;
+                foreach (Match match in rgx.Matches(textFormat)) {
+                    txt = txt.Replace(match.Value, dr.SafeRead(match.Result("$1"), ""));
+                }
+
+                //處理attribute
+                string attr = attrFormat;
+                foreach (Match match in rgx.Matches(attrFormat)) {
+                    attr = attr.Replace(match.Value, dr.SafeRead(match.Result("$1"), ""));
+                }
+
+                if (string.Compare(val, setValue, true) == 0)
+                    rtnStr += "<option value='" + val + "' selected " + attr + ">" + txt + "</option>\n";
+                else
+                    rtnStr += "<option value='" + val + "' " + attr + ">" + txt + "</option>\n";
+            }
+        }
+        return rtnStr;
+    }
 	#endregion
 
 	#region 產生Option字串 +static string Option(DataTable)
