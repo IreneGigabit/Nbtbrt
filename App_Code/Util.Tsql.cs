21dced7dc1b926cc729d8b8fe8078ce5f711f919
IreneGigabit <33124327+IreneGigabit@users.noreply.github.com>
2020-09-30 17:41:18 +0800

    Update Util.Tsql.cs

--- App_Code/Util.Tsql.cs
+++ App_Code/Util.Tsql.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Web;
 
@@ -8,14 +8,15 @@ public static partial class Util
     /// <summary>
     /// 串接insert語法
     /// </summary>
-    public static string GetInsertSQL(string TableName, List<DBColumn> DBColumn) {
+    public static string GetInsertSQL(List<DBColumn> DBColumn) {
         string strCol = "", strValue = "";
         foreach (DBColumn col in DBColumn) {
             string value = HttpContext.Current.Request[col.RequestName];
-            //有資料,或有給值
-            if ((value != null && value != "") || (col.EmptyValue != null && col.EmptyValue != "") || col.ColType == ColType.Value) {
+            //if ((value != null && value != "") || (col.EmptyValue != null && col.EmptyValue != "") || col.ColType == ColType.Value) {
+            //if ((value != null && value != "") || (!col.IsNullNoSave)) {
+            if ((value != null && value != "") || (col.IsNullValue != null && col.IsNullValue != "") || !col.IsNullNoSave || col.ColType == ColType.Value) {
                 //HttpContext.Current.Response.Write("**" + col.RequestName + "<BR>");
-                if (DBColumn.IndexOf(col) != 0) {
+                if (strCol != "") {
                     strCol += ", ";//欄位
                     strValue += ", ";//資料
                 }
@@ -24,41 +25,79 @@ public static partial class Util
                 strCol += col.DBColName;
 
                 //串接資料
-                if (value == null && (col.EmptyValue != null && col.EmptyValue != "")) {
-                    //沒有submit欄位但有指定空值
-                    strValue += col.EmptyValue;
-                }
-                else {
-                    switch (col.ColType) {
-                        case ColType.Str:
-                            strValue += "'" + value.Replace("'", "''") + "'";
-                            break;
-                        case ColType.PStr:
-                        case ColType.Date:
-                            if (value == null || value == "")
-                                strValue += "null";
-                            else
-                                strValue += "'" + value.Replace("'", "''") + "'";
-                            break;
-                        case ColType.Number:
-                            //HttpContext.Current.Response.Write(col.DBColName + "," + value + "<BR>");
-                            if (value == null || value == "")
-                                strValue += "null";
-                            else
-                                strValue += "" + value.Replace("'", "''") + "";
-                            break;
-                        case ColType.Value:
-                            strValue += col.RequestName.Replace("'", "''");
-                            break;
-                        default:
-                            strValue += "'" + value.Replace("'", "''") + "'";
-                            break;
-                    }
-                }
+                strValue += ParseValue(col);
             }
         }
 
-        return string.Format("insert into {0}({1})values({2})", TableName, strCol, strValue);
+        return string.Format("({0})values({1})", strCol, strValue);
+    }
+    #endregion
+
+    #region 串接update語法
+    /// <summary>
+    /// 串接update語法
+    /// </summary>
+    public static string GetUpdateSQL(List<DBColumn> DBColumn) {
+        string strSet = "";
+        foreach (DBColumn col in DBColumn) {
+            string value = HttpContext.Current.Request[col.RequestName];
+            //if ((value != null && value != "") || (col.EmptyValue != null && col.EmptyValue != "") || col.ColType == ColType.Value) {
+            if ((value != null && value != "") || (col.IsNullValue != null && col.IsNullValue != "") || !col.IsNullNoSave || col.ColType == ColType.Value) {
+                //HttpContext.Current.Response.Write("**" + col.RequestName + "<BR>");
+                if (strSet != "") {
+                    strSet += ", ";
+                }
+
+                //串接欄位
+                strSet += col.DBColName;
+
+                //串接資料
+                strSet += "=" + ParseValue(col);
+            }
+        }
+
+        return strSet;
+    }
+    #endregion
+
+    #region 依ColType型態轉值
+    /// <summary>
+    /// 依ColType型態轉值
+    /// </summary>
+    private static string ParseValue(DBColumn col) {
+        string strRtn = "";
+        string value = HttpContext.Current.Request[col.RequestName];
+
+        if ((value == null || value == "") && (col.IsNullValue != null && col.IsNullValue != "")) {
+            //沒有值,但有指定預設值
+            strRtn = col.IsNullValue.ToBig5().Trim();
+        } else {
+            value = (value ?? "").Replace("'", "''").ToBig5().Trim();
+            switch (col.ColType) {
+                case ColType.Str:
+                    strRtn = "'" + value + "'";
+                    break;
+                case ColType.Null:
+                    if (value == null || value == "")
+                        strRtn = "null";
+                    else
+                        strRtn = "'" + value + "'";
+                    break;
+                case ColType.Zero:
+                    if (value == null || value == "")
+                        strRtn = "0";
+                    else
+                        strRtn = "" + value + "";
+                    break;
+                case ColType.Value:
+                    strRtn = "" + col.RequestName.ToBig5().Trim();
+                    break;
+                default:
+                    strRtn = "'" + value + "'";
+                    break;
+            }
+        }
+        return strRtn;
     }
     #endregion
 }
@@ -69,17 +108,29 @@ public class DBColumn
     public string DBColName { get; set; }
     public string RequestName { get; set; }
     public ColType ColType { get; set; }
-    public string EmptyValue { get; set; }
+    public string IsNullValue { get; set; }
+    public bool IsNullNoSave { get; set; }
 
     /// <summary>
-    /// 無指定型態則為ColType.PStr
+    /// 無指定型態則為ColType.Null
     /// </summary>
     /// <param name="dbColumnName">資料庫欄位名稱</param>
     /// <param name="requestName">畫面欄位名稱</param>
     public DBColumn(string dbColumnName, string requestName) {
         this.DBColName = dbColumnName;
         this.RequestName = requestName;
-        this.ColType = ColType.PStr;
+        this.ColType = ColType.Null;
+        this.IsNullNoSave = false;
+    }
+
+    /// <param name="dbColumnName">資料庫欄位名稱</param>
+    /// <param name="requestName">畫面欄位名稱/指定值</param>
+    /// <param name="isNullNoSave">無值時不產生update/insert語法</param>
+    public DBColumn(string dbColumnName, string requestName, bool isNullNoSave) {
+        this.DBColName = dbColumnName;
+        this.RequestName = requestName;
+        this.ColType = ColType.Null;
+        this.IsNullNoSave = isNullNoSave;
     }
 
     /// <param name="dbColumnName">資料庫欄位名稱</param>
@@ -89,17 +140,30 @@ public class DBColumn
         this.DBColName = dbColumnName;
         this.RequestName = requestName;
         this.ColType = dbColumnType;
+        this.IsNullNoSave = false;
     }
 
     /// <param name="dbColumnName">資料庫欄位名稱</param>
     /// <param name="requestName">畫面欄位名稱/指定值</param>
     /// <param name="dbColumnType">指定欄位型態</param>
-    /// <param name="emptyValue">無request欄位時寫入指定值</param>
-    public DBColumn(string dbColumnName, string requestName, ColType dbColumnType,string emptyValue) {
+    /// <param name="isNullValue">無值時寫入指定值</param>
+    public DBColumn(string dbColumnName, string requestName, ColType dbColumnType,string isNullValue) {
         this.DBColName = dbColumnName;
         this.RequestName = requestName;
         this.ColType = dbColumnType;
-        this.EmptyValue = emptyValue;
+        this.IsNullNoSave = false;
+        this.IsNullValue = isNullValue;
+    }
+
+    /// <param name="dbColumnName">資料庫欄位名稱</param>
+    /// <param name="requestName">畫面欄位名稱/指定值</param>
+    /// <param name="dbColumnType">指定欄位型態</param>
+    /// <param name="isNullNoSave">無值時不產生update/insert語法</param>
+    public DBColumn(string dbColumnName, string requestName, ColType dbColumnType, bool isNullNoSave) {
+        this.DBColName = dbColumnName;
+        this.RequestName = requestName;
+        this.ColType = dbColumnType;
+        this.IsNullNoSave = isNullNoSave;
     }
 }
 
@@ -108,19 +172,15 @@ public enum ColType
     /// <summary>
     /// 無值時寫入null
     /// </summary>
-    PStr,
+    Null,
     /// <summary>
     /// 無值時寫入''
     /// </summary>
     Str,
     /// <summary>
-    /// 無值時寫入null
+    /// 無值時寫入0
     /// </summary>
-    Date,
-    /// <summary>
-    /// 無值時寫入null
-    /// </summary>
-    Number,
+    Zero,
     /// <summary>
     /// 不使用畫面欄位,直接指定入值
     /// </summary>
